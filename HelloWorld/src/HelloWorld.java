ccc0cf060f830d7f564cc3a0e61c58a80872e7b9
Hito <h0925t@gmail.com>
2013-04-25 22:57:41 +0900

    First Commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Hito
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args CommandLine Arguments
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!"); //$NON-NLS-1$
+	}
+
+}
